87d32ac68c4d6c62b060406cbd3105bf7a5ce40c
yangyang65 <yangyang65@jd.com>
2018-11-02 22:00:22 +0800

    add shell scripts and modify db addr for env product

--- doc/dynamodb_table/dynamodb_command.sh
+++ doc/dynamodb_table/dynamodb_command.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-aws dynamodb list-tables --endpoint-url http://127.0.0.1:8000
-aws dynamodb list-tables --endpoint-url https://dynamodb.cn-northwest-1.amazonaws.com.cn
+#aws dynamodb list-tables --endpoint-url http://127.0.0.1:8000
+#aws dynamodb list-tables --endpoint-url https://dynamodb.cn-northwest-1.amazonaws.com.cn
+#
+#aws dynamodb get-item --table-name zx_p --key '{"gid":{"S":"spuid1"}, "sid":{"S": "0"} }' --endpoint-url http://127.0.0.1:8000
+aws dynamodb get-item --table-name zx_p --key '{"gid":{"S":"9a34d393f279f4ba"}, "sid":{"S": "0"} }'
--- doc/login_aizhenxuan.sh
+++ doc/login_aizhenxuan.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 #slave
-mysql -hzxcore.cluster-ro-cj8zasm1k9qa.rds.cn-northwest-1.amazonaws.com.cn -P3306 -uzxTradeRoot -pt1h2x344! -DzxTrade
+#mysql -hzxcore.cluster-ro-cj8zasm1k9qa.rds.cn-northwest-1.amazonaws.com.cn -P3306 -uzxTradeRoot -pt1h2x344! -DzxTrade
+
+mysql -hnewzxcoretest1.cj8zasm1k9qa.rds.cn-northwest-1.amazonaws.com.cn -P3306 -uroot1234 -proot1234
